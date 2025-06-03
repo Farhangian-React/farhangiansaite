a53833ca44310c6a0da02957920062c4ca39d856
farhangin-elham <elhamfarhangian6@gmail.com>
2025-06-04 01:38:22 +0330

    site12

--- src/Aboutus/Aboutus.js
+++ src/Aboutus/Aboutus.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React,{useEffect,useState} from 'react';
 import imageaboutus from "../img/image.gif";
 import { IoLogoWhatsapp } from "react-icons/io";
 import { FaTelegram } from "react-icons/fa";
@@ -8,7 +8,32 @@ import urlpdf from "../Rezomeh/Farhangian-rezomeh.pdf";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "../Style.css";
+
+
 function Aboutus() {
+  const useTypewriter = (text, speed) => {
+  const [displayText, setDisplayText] = useState("");
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (index === text.length) {
+        clearTimeout(timeoutId);
+        return;
+      }
+
+      setDisplayText((prevText) => prevText + text.charAt(index));
+      setIndex((prevIndex) => prevIndex + 1);
+    }, speed);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [text, speed, index]);
+
+  return displayText;
+};
+
  useEffect(()=> {
       AOS.init({
         disable: "phone",
@@ -17,21 +42,17 @@ function Aboutus() {
       })
   },[]);
 
-
+  const text = useTypewriter("سال 90 برنامه نویسی را با زبان های c++ , java شروع کردم و از سال 1400 مشغول توسعه ی وب سایت ها به صورت حرفه ای می باشم و به ایجاد راهکارهای دیجیتال کاربردی علاقه دارم و همیشه به دنبال بهبود و نوآوری در کارها هستم.آماده ی همکاری با تیم ها و افرادی هستم که به رشد و توسعه اهمیت می دهند."
+    , 100);
   return (
     <>
 <div className='flex flex-col md:flex-row-reverse h-[70vh] justify-center mx-auto my-10 '>
-    <div data-aos="flip-left"
-     data-aos-easing="ease-out-cubic"
-     data-aos-duration="2000" dir='rtl' className='flex flex-col justify-start md:justify-center bg-transparent w-[95%] md:w-[50%] z-10 mx-auto px-5 h-[90%]'>
+    <div dir='rtl' className='flex flex-col justify-start md:justify-center bg-transparent w-[95%] md:w-[50%] z-10 mx-auto px-5 h-[90%]'>
         <p className='text-lg text-[#f9f9f9] text-center '> 
 Front-end developer
 </p>
 <p className='text-lg md:text-2xl text-[#8229d5] text-center font-bold pb-2'> الهام فرهنگیان توسعه دهنده ی وب</p>
-<p className='text-sm md:text-md text-[#f9f9f9] text-justify font-extralight px-5 pt-5 '>  سال 90 برنامه نویسی را با زبان های c++ , java  شروع کردم و از سال 1400 مشغول توسعه ی وب سایت ها به صورت حرفه ای میباشم
-  و به ایجاد  
-  راهکارهای دیجیتال کاربردی علاقه دارم و همیشه به دنبال بهبود
-   و نوآوری در کارها هستم. آماده‌ی همکاری با تیم‌ها و افرادی هستم که به رشد و توسعه اهمیت می‌دهند. </p>
+<p className='text-sm md:text-md text-[#f9f9f9] text-justify font-extralight px-5 pt-5 '>  {text} </p>
 <p className=''></p>
  <div className='flex flex-row justify-center  mt-8 mr-2 md:mr-5  h-[45px] z-10'>
   <a href={urlpdf} target='_self'><button className='button1  ' type='submit' >  <span className='formbutton1'> دانلود رزومه</span></button></a>
